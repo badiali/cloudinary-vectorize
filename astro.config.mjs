d3b5784a0701a9211482847f052e6e54ee94e7cb
Luis Badiali <luis@badiali.es>
2023-02-26 19:22:13 +0100

    Integro Svelte con Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import svelte from "@astrojs/svelte";
 
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [tailwind(), svelte()],
 });
