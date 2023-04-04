982a0d9ae167d1ff4bd57bef3285ce0c2e22c51a
Luis Badiali <luis@badiali.es>
2023-04-04 21:50:42 +0200

    Organización layout

--- src/components/Steps.svelte
+++ src/components/Steps.svelte
@@ -5,14 +5,21 @@
 	import StepEdit from "../components/StepEdit.svelte";
 </script>
 
-{#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING}
+<div
+	class="w-full flex flex-col lg:flex-row items-center justify-between gap-8 px-6 xl:px-0"
+>
 	<div
-		class="w-full flex flex-col lg:flex-row items-center justify-between gap-8 px-6 xl:px-0"
+		class="flex flex-col lg:flex-col items-center lg:items-start md:max-w-3xl lg:max-w-lg"
 	>
-		<div
-			class="flex flex-col lg:flex-col items-center lg:items-start md:max-w-3xl lg:max-w-lg"
-		>
-			<video class="object-cover w-full aspect-[16:10] rounded-[2rem] mb-5" src="./process.mp4" poster="./poster.jpg" autoplay loop muted></video>
+		{#if $imageStatus === ImageStatus.READY || $imageStatus === ImageStatus.UPLOADING}
+			<video
+				class="object-cover w-full aspect-[16:10] rounded-[2rem] mb-5"
+				src="./process.mp4"
+				poster="./poster.jpg"
+				autoplay
+				loop
+				muted
+			/>
 			<h1
 				class="font-bold tracking-tight mb-3 text-4xl md:text-5xl lg:text-6xl text-center lg:!text-left"
 			>
@@ -22,12 +29,16 @@
 				100% Automatically and
 				<span class="text-gradient"> Free</span>
 			</p>
-			<p class="mb-0">By using this service, you can download your raster image as an SVG file, making it easier to edit and use for various design projects.</p>
-		</div>
-		<div class="relative flex flex-col gap-4 md:gap-8 max-w-md">
-			<StepUpload />
-		</div>
+			<p class="mb-0">
+				By using this service, you can download your raster image as an
+				SVG file, making it easier to edit and use for various design
+				projects.
+			</p>
+		{:else if $imageStatus === ImageStatus.DONE}
+			<StepEdit />
+		{/if}
 	</div>
-{:else if $imageStatus === ImageStatus.DONE}
-	<StepEdit />
-{/if}
+	<div class="relative flex flex-col gap-4 md:gap-8 max-w-md">
+		<StepUpload />
+	</div>
+</div>
